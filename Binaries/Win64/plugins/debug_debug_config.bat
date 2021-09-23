139ec01670361835740f72aefc1697915654e9fe
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2021-09-23 01:00:49 -0400

    Initial Commit

---
+++ Binaries/Win64/plugins/debug_debug_config.bat
@@ -0,0 +1,15 @@
+copy ".\EDFix_Debug_Win64.asi" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\EDFix_Debug_Win64.ilk" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\EDFix_Debug_Win64.pdb" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\config.ini" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\updater.exe" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\Updater.ilk" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\Updater.pdb" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+start steam://run/1039890
---
+++ Binaries/Win64/plugins/debug_release_config.bat
@@ -0,0 +1,7 @@
+copy ".\EDFix_Release_Win64.asi" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\config.ini" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+copy ".\updater.exe" "X:\Steam\steamapps\common\paper\EDFIR\Binaries\Win64\plugins"
+
+start steam://run/1039890
